90c84a5c3b6b4fc40b0fe9db5013182b9dc267b9
webdevbynight <git@victor-brito.dev>
2026-01-23 00:31:04 +0100

    Fixed comment

--- ts/index.ts
+++ ts/index.ts
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const rangeInput = form.querySelector<HTMLInputElement>("input[type=range]");
     if (rangeInput) {
       rangeInput.addEventListener("input", () => {
-        // Update the `--val` custom property (for Safari and Blink-based browsers to display the gradient as a progress track correctly)
+        // Update the `--value` custom property (for Safari and Blink-based browsers to display the gradient as a progress track correctly)
         rangeInput.style.setProperty("--value", rangeInput.value);
       });
     }

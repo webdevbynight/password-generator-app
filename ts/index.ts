896d57f56b7d4e4a0fe69a57c3d1dec4b8d8f18b
webdevbynight <git@victor-brito.dev>
2026-01-23 00:12:50 +0100

    Styled range input

--- ts/index.ts
+++ ts/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello, world! Does it work or does it suck?");
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("password-generator");
+  if (form) {
+    const rangeInput = form.querySelector<HTMLInputElement>("input[type=range]");
+    if (rangeInput) {
+      rangeInput.addEventListener("input", () => {
+        // Update the `--val` custom property (for Safari and Blink-based browsers to display the gradient as a progress track correctly)
+        rangeInput.style.setProperty("--value", rangeInput.value);
+      });
+    }
+  }
+});
